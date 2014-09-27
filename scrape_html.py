68178b35d0ab4776d72ac7a78f16e1aaa00062ba
Jun Takahashi <jtakahas@princeton.edu>
2014-09-27 01:04:38 -0400

    implements get_image function, applies to all entries from names.txt

--- scrape_html.py
+++ scrape_html.py
@@ -2,13 +2,27 @@ from bs4 import BeautifulSoup
 import sys
 import urlparse
 
-with open(sys.argv[1], 'r') as utsarga:
-	uts = utsarga.read()
-soup = BeautifulSoup(uts)
+with open(sys.argv[1], 'r') as originalhtml:
+	soup = BeautifulSoup(originalhtml.read())
+f = open('names.txt', 'r')
+names = f.readlines()
 
-# print (soup.prettify())
+def get_image (name):
+	img = soup.find('img', title=name)
+	if img != None:
+		return urlparse.urljoin("http://princeton.edu", img.get('src'))
+	else: 
+		print 'Error with: ' + name
 
-# for img in soup.find_all('img'):
-# 	print img
+# print (get_image("Utsarga Sikder"))
+# print (get_image("Junya Takahashi"))
 
-print urlparse.urljoin("http://princeton.edu", soup.find('img', title="Utsarga Sikder").get('src'))
+allnames = []
+for name in names:
+	s = name.partition(',')
+	# Put in format: First Last
+	fullname = str(s[2][1:].rstrip('\n') + ' ' + s[0])
+	allnames.append((fullname, get_image(fullname)))
+
+for entry in allnames:
+	print entry
